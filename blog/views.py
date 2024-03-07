89194477ebaaf607f7a57c60168ccc7863159087
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-07 11:38:29 +0900

    Add blog_list and blog_detail views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def blog_list(request):
+    return render(request, "blog_list.html")
+
+
+def blog_detail(request, id):
+    context = {"id": id}
+    return render(request, "blog_detail.html", context)
