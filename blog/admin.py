292e35d7fed5100725738d6f61ff26bb45db02b0
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-07 12:02:39 +0900

    Add BlogPostAdmin to blog/admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import BlogPost
+
+
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ("title", "content")
+
 
 # Register your models here.
+admin.site.register(BlogPost)
