74d857d301f6386dda3be073da5177ec8318e382
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-08 03:01:36 +0900

    Add PostCreateView to blog/urls.py and blog/views.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from blog.models import BlogPost
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 
 
 class BlogListView(ListView):
@@ -19,3 +19,13 @@ class BlogDetailView(DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         return context
+
+
+class PostCreateView(CreateView):
+    model = BlogPost
+    template_name = "blog/post_create.html"
+    fields = ["title", "category", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
