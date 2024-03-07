bf6f3ec4e2dd9385c3b9aa9c69d00533e60496ee
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-08 00:00:34 +0900

    Update static file paths in blog_detail.html and blog_list.html templates

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,5 @@ from blog.views import BlogListView, BlogDetailView
 
 urlpatterns = [
     path("", BlogListView.as_view(), name="blog_list"),
-    path("<int:id>/", BlogDetailView.as_view(), name="blog_detail"),
+    path("<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -18,5 +18,4 @@ class BlogDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["id"] = self.kwargs.get("id")
         return context
