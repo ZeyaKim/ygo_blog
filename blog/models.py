af99edce50cdad6550e0b2739c8053bdfe6a8bab
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-09 10:27:38 +0900

    Add createdat field to BlogPost model

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=200)
     category = models.CharField(max_length=100, default="General")
     content = models.TextField()
+    createdat = models.DateTimeField(auto_now_add=True)
 
     def __repr__(self):
         return self.title
