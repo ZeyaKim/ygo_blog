19e580ff33e53c9fad1f21bb19390459f1f45bd3
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-10 12:45:03 +0900

    Add RegisterView with UserCreationForm

--- main/views.py
+++ main/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import FormView
+
 
 class MainView:
     pass
 
-class RegisterView:
-    pass
+
+class RegisterView(FormView):
+    template_name = "register.html"
+    form_class = UserCreationForm
+    success_url = "login/"
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
