c88d9ccd686bbaaf63d179b9f8c556421ec727d2
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-11 04:15:55 +0900

    Add author field to BlogPost model

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 from django.utils import timezone
 
+from django.conf import settings
+
 
 # Create your models here.
 class BlogPost(models.Model):
     title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default="1"
+    )
     category = models.CharField(max_length=100, default="General")
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
