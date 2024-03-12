a25c1cb1f84cafb3816295048c3b0bd79f848b3a
ZeyaKim <chlwltjrdev@gmail.com>
2024-03-12 23:34:14 +0900

    Update max-width and link in index.html

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from main.views import RegisterView, LoginView, MainView, AccountView
 
 urlpatterns = [
-    path("", MainView.as_view(), name="main"),
+    path("", MainView.as_view(), name="index"),
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("account/<int:pk>/", AccountView.as_view(), name="account"),
